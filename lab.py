7e2dc7ac6b7cb0727ea28f6e976201552d123f54
shekhnaz <129167537+mooonalikaya@users.noreply.github.com>
2024-10-26 15:58:35 +0500

    Add files via upload

---
+++ lab.py
@@ -0,0 +1,56 @@
+# zadacha 1.1
+
+#i_input = input("Введите строку: ")
+#itog_list = list(i_input.lower())
+
+#print("Создан список:")
+#print(itog_list)
+
+
+
+#zadacha 1.2
+
+
+
+
+#zadacha 2.1
+# name = input("Имя студента: ")
+# last_name = input("Фамилия студента: ")
+# assignment_scores = input("Баллы за задания (через запятую): ").split(',')
+# lab_scores = input("Баллы за лабки (через запятую): ").split(',')
+# test_scores = input("Баллы за тесты (через запятую): ").split(',')
+#
+#
+# student = {
+#     'name': name,
+#     'last_name':last_name,
+#     'assignment': assignment_scores,
+#     'lab': lab_scores,
+#     'test': test_scores
+# }
+# print("студент =", student)
+#
+
+#zadacha 2.2
+# student = {
+#     'name': 'Shekhnaz',
+#     'assignment': [82, 56, 30, 100],
+#     'test': [78, 77,
+#     'lab': [78, 77],
+# }
+#
+# all_submitted = (
+#     len(student['assignment']) == 4 and
+#     len(student['test']) == 2 and
+#     len(student['lab']) == 2
+# )
+#
+# if all_submitted:
+#     print(f"{student['name']} сдал все задания.")
+# else:
+#     print(f"{student['name']} не сдал все задания.")
+#
+
+
+#zadacha 2.3
+
